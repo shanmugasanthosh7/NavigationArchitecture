b478421965099c60261cc87633f9717e81398296
Santhosh <shanmugasanthosh@outlook.com>
2018-05-15 00:40:53 +0530

    Added Navigation pop using Up button

--- app/src/main/java/com/aptus/navigatioarchitecture/MainActivity.kt
+++ app/src/main/java/com/aptus/navigatioarchitecture/MainActivity.kt
@@ -4,18 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
 
-class MainActivity : AppCompatActivity(),NavigationFragment.OnFragmentInteractionListener,BlankFragment.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity(), NavigationFragment.OnFragmentInteractionListener, BlankFragment.OnFragmentInteractionListener {
 
     override fun onFragmentInteraction() {
-        Navigation.findNavController(this,R.id.my_nav_host_fragment).navigate(R.id.action_navigationFragment_to_blankFragment)
+        Navigation.findNavController(this, R.id.my_nav_host_fragment).navigate(R.id.action_navigationFragment_to_blankFragment)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return Navigation.findNavController(this,R.id.my_nav_host_fragment).navigateUp()
+    override fun onSupportNavigateUp(): Boolean {// Up button will on this method
+        return Navigation.findNavController(this, R.id.my_nav_host_fragment).navigateUp()
     }
 }
